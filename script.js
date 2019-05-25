a33c7a91b45b607f19cd9dfdc1f140aabee93df7
Amudski <svalebaek3@gmail.com>
2019-05-25 19:32:25 +0200

    Øh...Sidste commit for i dag. d. 25 maj.

--- script.js
+++ script.js
@@ -27,15 +27,3 @@ function toggleMenu() {
 //---------------------------------BURGER MENU SLUT----------------------
 
 //---------------------------------LOGIN SKÆRM------------------------
-
-function clickMaterialer() {
-    console.log("clickMaterialer");
-    document.querySelector(".flexwrapper").addEventListener("click", clickMaterialer.html);
-}
-
-
-//---------------------------------LOGIN SKÆRM SLUT------------------------
-
-function myFunction() {
-    document.querySelector("flexwrapper").addEventListener location.replace("click", "https://www.w3schools.com")
-}
