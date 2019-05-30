316d1deb465f956356582cc030ed1e8d21df17d9
Amudski <svalebaek3@gmail.com>
2019-05-30 17:09:50 +0200

    Last commit.

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function myFunction() {
         x.style.display = "block";
     }
 }
+
+
+// Javascript Kalender
+function myFunctionskema() {
+
+    let x = document.querySelector("#myLinksskema");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
